44610b9b64aa7dc838c77b2ab51aa7a8b26ae0dd
AlejandroPonceG <71816760+AlejandroPonceG@users.noreply.github.com>
2021-03-18 21:57:17 -0500

    Full App

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,341 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Calculator',
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeState extends State<Home> {
+  List numbers = [];
+  List sorted = [];
+  num n, mayor, menor, avg;
+  String value;
+  int group = 1;
+  String result = "";
+  String collection = "";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Colors.white,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          padding: EdgeInsets.fromLTRB(
+            MediaQuery.of(context).size.width * 0.07,
+            MediaQuery.of(context).size.height * 0.2,
+            MediaQuery.of(context).size.width * 0.07,
+            MediaQuery.of(context).size.height * 0.03,
+          ),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      margin: EdgeInsets.only(top: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xffF8F8F8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            offset: Offset(0.0, 1.0), //(x,y)
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {
+                          setState(() {
+                            if (value.isEmpty) {
+                              n = n;
+                            } else {
+                              n = int.parse(value);
+                            }
+                          });
+                        },
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Insert number",
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                            )),
+                      )),
+                  Padding(padding: EdgeInsets.only(top: 25)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.06,
+                        child: Center(
+                          child: RaisedButton(
+                            onPressed: () {
+                              setState(() {
+                                if (n == null) {
+                                  print("hola");
+                                } else {
+                                  numbers.add(n);
+                                  collection = numbers.toString();
+                                }
+                              });
+                            },
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.blue,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Add to Collection",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "Roboto",
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.04,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.06,
+                        child: Center(
+                          child: RaisedButton(
+                            onPressed: () {
+                              setState(() {
+                                numbers.clear();
+                                collection = numbers.toString();
+                              });
+                            },
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.blue,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Delete Collection",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "Roboto",
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.04,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.05)),
+                  Container(
+                      margin: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.03),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: Text("Collection",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.10,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10),
+                          ),
+                          Center(
+                            child: Text(collection,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.04,
+                                  fontWeight: FontWeight.normal,
+                                )),
+                          ),
+                        ],
+                      )),
+                  Padding(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.05)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(children: [
+                        Radio(
+                            value: 1,
+                            groupValue: group,
+                            onChanged: (value) {
+                              setState(() {
+                                group = value;
+                              });
+                            }),
+                        Text("Bigger-num"),
+                      ]),
+                      Column(children: [
+                        Radio(
+                            value: 2,
+                            groupValue: group,
+                            onChanged: (value) {
+                              setState(() {
+                                group = value;
+                              });
+                            }),
+                        Text("minor-num")
+                      ]),
+                      Column(children: [
+                        Radio(
+                            value: 3,
+                            groupValue: group,
+                            onChanged: (value) {
+                              setState(() {
+                                group = value;
+                              });
+                            }),
+                        Text("Order"),
+                      ]),
+                      Column(children: [
+                        Radio(
+                            value: 4,
+                            groupValue: group,
+                            onChanged: (value) {
+                              setState(() {
+                                group = value;
+                              });
+                            }),
+                        Text("Average"),
+                      ]),
+                    ],
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.05)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.08,
+                        child: Center(
+                          child: RaisedButton(
+                            onPressed: () {
+                              mayor = numbers[0];
+                              menor = numbers[numbers.length - 1];
+                              avg = 0;
+                              num suma = 0;
+                              setState(() {
+                                if (group == 1) {
+                                  for (int x = 0; x < numbers.length; x++) {
+                                    if (numbers[x] > mayor) {
+                                      mayor = numbers[x];
+                                    }
+                                  }
+                                  result = mayor.toString();
+                                } else if (group == 2) {
+                                  for (int x = 0; x < numbers.length; x++) {
+                                    if (menor > numbers[x]) {
+                                      menor = numbers[x];
+                                    }
+                                  }
+                                  result = menor.toString();
+                                } else if (group == 3) {
+                                  setState(() {
+                                    numbers.sort();
+                                    result = numbers.toString();
+                                  });
+                                } else if (group == 4) {
+                                  for (int x = 0; x < numbers.length; x++) {
+                                    suma += numbers[x];
+                                    avg = suma / numbers.length;
+                                  }
+                                  result = avg.toStringAsFixed(2);
+                                }
+                              });
+                            },
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.blue,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text("Calculate",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "Roboto",
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.05,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.height * 0.02),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(
+                                child: Text("Result",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.10,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                              Center(
+                                child: Text(result,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize:
+                                          MediaQuery.of(context).size.width *
+                                              0.04,
+                                      fontWeight: FontWeight.normal,
+                                    )),
+                              ),
+                            ],
+                          )),
+                    ],
+                  ),
+                ]),
+          ),
+        ));
   }
 }
